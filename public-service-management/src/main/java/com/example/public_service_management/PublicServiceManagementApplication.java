5e5243158d84e46d1b4a71d65b9056c558c8878c
hanhdt-1783 <doan.thi.hanh@sun-asterisk.com>
2025-11-06 10:24:01 +0700

    Initialize the project

---
+++ public-service-management/src/main/java/com/example/public_service_management/PublicServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.public_service_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicServiceManagementApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PublicServiceManagementApplication.class, args);
+  }
+
+}
