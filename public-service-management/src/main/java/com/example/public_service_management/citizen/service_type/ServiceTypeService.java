3ea3867b34eb57cb313a899c795898953e142caa
hanhdt-1783 <doan.thi.hanh@sun-asterisk.com>
2025-11-14 16:57:16 +0700

    [Citizen] Get service type details API

--- public-service-management/src/main/java/com/example/public_service_management/citizen/service_type/ServiceTypeService.java
+++ public-service-management/src/main/java/com/example/public_service_management/citizen/service_type/ServiceTypeService.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.public_service_management.common.dto.PageResDto;
+import com.example.public_service_management.common.exceptions.NotFoundException;
+import com.example.public_service_management.common.utils.I18nUtil;
+import com.example.public_service_management.service_type.ServiceType;
 import com.example.public_service_management.service_type.ServiceTypeRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,11 +18,19 @@ import lombok.RequiredArgsConstructor;
 public class ServiceTypeService {
   private final ServiceTypeRepository serviceTypeRepository;
   private final ModelMapper modelMapper;
+  private final I18nUtil i18nUtil;
 
   public PageResDto<GetServiceTypeListResDto> getList(Long categoryId, Pageable pageable) {
     Page<GetServiceTypeListResDto> serviceTypes = serviceTypeRepository.findByCategoryId(categoryId, pageable)
         .map(st -> modelMapper.map(st, GetServiceTypeListResDto.class));
 
     return new PageResDto<GetServiceTypeListResDto>(serviceTypes);
+  }
+
+  public GetServiceTypeDetailsResDto getDetails(Long serviceTypeId) {
+    ServiceType serviceType = serviceTypeRepository.findById(serviceTypeId)
+        .orElseThrow(() -> new NotFoundException(i18nUtil.get("error.service_type.not_found", serviceTypeId)));
+
+    return modelMapper.map(serviceType, GetServiceTypeDetailsResDto.class);
   }
 }
